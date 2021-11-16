76c6289bf7987c69a576aeac3b860ceb867e2493
sleepingming <daniltop1@bk.ru>
2021-11-16 21:49:39 +0300

    task1v.2

--- 1.rb
+++ 1.rb
@@ -1,85 +1,85 @@
 class Station
- attr_accessor :trains, :train_types
- attr_reader :name
- def initialize(name)
-  @name = name
-  @trains = []
-  @train_types = []
- end
+  attr_accessor :trains
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
 
- def train_arrive(train)
-  self.trains << train
-  self.train_types << train.type
- end
+  def train_arrive(train)
+    self.trains << train
+  end
 
- def send_train(train)
-  self.trains.delete(train)
-  self.train_types(train.type)
- end
+  def send_train(train)
+    self.trains.delete(train)
+  end
 
- def method_name
-
- end
 end
 
 
 
 class Route
- attr_accessor :first, :last, :stations
- def initialize(first, last)
-  @first = first
-  @last = last
-  @stations = [first,last]
- end
+  attr_accessor :first, :last, :stations
+  def initialize(first, last)
+    @stations = [first,last]
+  end
 
- def add_station(station_name)
-  self.stations.insert(1, station_name)
- end
+  def add_station(station)
+    self.stations.insert(1, station)
+  end
 
- def delete_station(station_name)
-  self.stations.delete(station_name) if (station_name != first)&&(station_name != last)
- end
+  def delete_station(station)
+    self.stations.delete(station) if (station != first) && (station != last)
+  end
 
 end
 
 
 class Train
- attr_reader :route
- attr_accessor :speed, :length, :route, :station, :stationprev, :stationnext
- def initialize(number, type, length)
-  @number = number
-  @type = type
-  @length = length
-  @speed = 0
- end
-
- def stop
-  self.speed = 0
- end
-
- def change_length(count)
-  self.length += count if (self.speed == 0)&&((count == 1)||(count == -1))
- end
-
- def change_route(route)
-  self.route = route
-  self.station = route.stations[0]
-  @index = 0
- end
-
- def change_station(count)
-  if ((count == 1)||(count == -1))
-   self.station = route.stations[@index+count]
-   @index+=count
+  attr_reader :route
+  attr_accessor :speed, :length, :route, :station, :stationprev, :stationnext, :stationcurrent
+  def initialize(number, type, length)
+    @number = number
+    @type = type
+    @length = length
+    @speed = 0
   end
- end
 
- def prev_station
-  self.stationprev = route.stations[@index - 1]
- end
+  def stop
+    self.speed = 0
+  end
 
- def next_station
-  self.stationnext = route.stations[@index + 1]
- end
+  def inc_length
+    self.length += 1 if (self.speed == 0)
+  end
+
+  def dec_length
+    self.length -= 1 if (self.speed == 0)
+  end
+
+  def change_route(route)
+    self.route = route
+    self.station = route.stations[0]
+  end
+
+  def current_station
+    self.stationcurrent = route.stations[route.stations.index(station)]
+  end
+
+  def prev_station
+    self.stationprev = route.stations[route.stations.index(station) - 1] if (station != route.first)
+  end
+
+  def next_station
+    self.stationnext = route.stations[route.stations.index(station) + 1] if (station != route.last)
+  end
+
+  def to_next_station
+    self.station = route.stations[route.stations.index(station) + 1] if (station != route.last)
+  end
+
+  def to_prev_station
+    self.station = route.stations[route.stations.index(station) - 1] if (station != route.first)
+  end
 
 end
